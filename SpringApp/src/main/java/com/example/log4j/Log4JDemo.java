b61e3799adc99a71be42e196dd3bfe9ab54c09fa
Maulik Patel <maulikp55@gmail.com>
2015-09-20 13:18:59 +0530

    SpringApp :: Re-Structre For Log4j Demo

--- SpringApp/src/main/java/com/example/log4j/Log4JDemo.java
+++ SpringApp/src/main/java/com/example/log4j/Log4JDemo.java
@@ -1,11 +1,11 @@
-package com.springframework.example.test;
+package com.example.log4j;
 
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
-public class Log4JTestDemo {
+public class Log4JDemo {
 
-	private static Logger logger = LogManager.getLogger(Log4JTestDemo.class);
+	private static Logger logger = LogManager.getLogger(Log4JDemo.class);
 
 	public static void main(String[] args) {
 		logger.info("This Is My First Log4j Info Demo...!!!");
